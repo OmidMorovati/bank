cea194713b0c33590577bb949e6947780a6b5699
omid morovati <livermid@live.com>
2023-07-16 18:47:53 +0330

    FIX: AccountResource namespace

--- app/Http/Resources/Customer/RegisterResource.php
+++ app/Http/Resources/Customer/RegisterResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Customer;
 
+use App\Http\Resources\Account\AccountResource;
 use App\Models\Customer;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
